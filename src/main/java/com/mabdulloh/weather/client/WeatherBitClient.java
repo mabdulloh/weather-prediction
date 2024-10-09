ddc3aba276de25bbac65a0c49f754258721c7631
Muhammad Abdulloh <muhammad.abdulloh@kuelap.io>
2024-10-09 12:49:06 +0700

    tyding up

--- src/main/java/com/mabdulloh/weather/client/WeatherBitClient.java
+++ src/main/java/com/mabdulloh/weather/client/WeatherBitClient.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mabdulloh.weather.domain.weatherbit.CurrentWeatherBitClientResponse;
 import com.mabdulloh.weather.exception.WeatherException;
+import com.mabdulloh.weather.util.Mapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
@@ -16,7 +17,6 @@ public class WeatherBitClient implements WeatherClient<CurrentWeatherBitClientRe
     private final static String API_KEY = "";
     private final static String BASE_URL = "https://api.weatherbit.io/v2.0";
     private final static RestTemplate REST_TEMPLATE = new RestTemplate();
-    private final static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
 
     @Override
@@ -24,9 +24,9 @@ public class WeatherBitClient implements WeatherClient<CurrentWeatherBitClientRe
         try {
             final var url = BASE_URL + "/current" + "?city=" + cityName + "&country=" + countryCode + "&key=" + API_KEY;
             final var response = REST_TEMPLATE.getForEntity(url, String.class);
-            final var data = OBJECT_MAPPER.readTree(response.getBody()).get("data");
-            return OBJECT_MAPPER.convertValue(data, CurrentWeatherBitClientResponse[].class)[0];
-        } catch (HttpClientErrorException | JsonProcessingException e) {
+            final var data = Mapper.getJsonNode(response.getBody(), "data");
+            return Mapper.fromJson(data, CurrentWeatherBitClientResponse[].class)[0];
+        } catch (HttpClientErrorException e) {
             throw new WeatherException(e.toString());
         }
     }
